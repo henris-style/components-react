424d68949fd1092050ed4cf3559289b1faef3074
Sil van Diepen <me@silvandiepen.com>
2019-12-19 09:17:30 +0100

    init

---
+++ src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@henris/components';
+
+import { defineCustomElements, applyPolyfills } from '@henris/components/loader';
+
+applyPolyfills().then(() => { defineCustomElements(window); });
+export const HenrisButton = /*@__PURE__*/createReactComponent<JSX.HenrisButton, HTMLHenrisButtonElement>('henris-button');
+export const HenrisInputText = /*@__PURE__*/createReactComponent<JSX.HenrisInputText, HTMLHenrisInputTextElement>('henris-input-text');
