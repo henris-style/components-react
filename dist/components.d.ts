cc67201ad5a688e12aa5255d7d9393cbe0c5d7e0
Sil van Diepen <me@silvandiepen.com>
2019-12-19 09:25:24 +0100

    build version

---
+++ dist/components.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { JSX } from '@henris/components';
+export declare const HenrisButton: import("react").ForwardRefExoticComponent<JSX.HenrisButton & import("react").HTMLAttributes<HTMLHenrisButtonElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLHenrisButtonElement>>;
+export declare const HenrisInputText: import("react").ForwardRefExoticComponent<JSX.HenrisInputText & import("react").HTMLAttributes<HTMLHenrisInputTextElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLHenrisInputTextElement>>;
---
+++ dist/components.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements, applyPolyfills } from '@henris/components/loader';
+applyPolyfills().then(() => { defineCustomElements(window); });
+export const HenrisButton = /*@__PURE__*/ createReactComponent('henris-button');
+export const HenrisInputText = /*@__PURE__*/ createReactComponent('henris-input-text');
+//# sourceMappingURL=components.js.map
